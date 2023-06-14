7dc7e7335fb2ef0c59f9baa2b2a898df01bd4c65
M-C-L <102340042+M-C-L@users.noreply.github.com>
2023-06-14 17:46:24 +0900

    Add files via upload
    Fix: api 연동 문제 해결
    
    aws를 이용해 http api를 불러 오지 못하는 문제를 해결
    fetch 하는 url 변경
    
    Feat: 통계성 혼잡도 api 연동
    
    통계성 혼잡도 연동에 성공해서 2,3호선을 제외한 나머지 호선에서 역을 클릭했을때 통계성 혼잡도 정보를 콘솔창에 출력하는 기능 추가

---
+++ statisticcongestion.js
@@ -0,0 +1,15 @@
+function statisticcongestion_info(stationID,day,sHour) {
+    
+    const options = {
+        method: 'GET',
+        //mode: 'no-cors',
+        headers: {accept: 'application/json', appkey: 'WCfyvYzuLu6HjI65CWiMe4ApH0zgqk9Y5dUoFRet'}
+        };
+
+    const url = `https://apis.openapi.sk.com/puzzle/congestion-train/stat/stations/${stationID}?dow=${day}&hh=${sHour}`
+    fetch(url,options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+    }
